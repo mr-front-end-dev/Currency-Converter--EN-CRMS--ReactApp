59348cf3294552c7c5d8df3ab4611fbf24a382c0
MYKYTA RYSIEV <nickpolska21@gmail.com>
2021-06-18 06:31:19 +0200

    Added currencies (short, name, rate)

---
+++ src/App/currencies.js
@@ -0,0 +1,177 @@
+export const currencies = [
+  {
+    short: "THB",
+    name: "Thailandian bat",
+    rate: 0.1237,
+  },
+  {
+    short: "USD",
+    name: "American dollar",
+    rate: 3.7031,
+  },
+  {
+    short: "AUD",
+    name: "Australian dollar",
+    rate: 2.8569,
+  },
+  {
+    short: "HKD",
+    name: "Hongkong dollar",
+    rate: 0.4777,
+  },
+  {
+    short: "CAD",
+    name: "Canadian dollar",
+    rate: 2.9073,
+  },
+  {
+    short: "NZD",
+    name: "New Zealand dollar",
+    rate: 2.6710,
+  },
+  {
+    short: "SGD",
+    name: "Singapore dollar",
+    rate: 2.8076,
+  },
+  {
+    short: "EUR",
+    name: "European euro",
+    rate: 4.5446,
+  },
+  {
+    short: "HUF_100",
+    name: "100 Hungarian forint",
+    rate: 1.2577,
+  },
+  {
+    short: "CHF",
+    name: "Swiss franc",
+    rate: 4.2078,
+  },
+  {
+    short: "GBP",
+    name: "British pound",
+    rate: 5.0261,
+  },
+  {
+    short: "UAH",
+    name: "Ukrainian hryvnia",
+    rate: 0.1304,
+  },
+  {
+    short: "JPY_100",
+    name: "100 Japanese yen",
+    rate: 3.6013,
+  },
+  {
+    short: "CZK",
+    name: "Czech krone",
+    rate: 0.1735,
+  },
+  {
+    short: "DKK",
+    name: "Danish krone",
+    rate: 0.6109,
+  },
+  {
+    short: "ISK_100",
+    name: "100 Icelandic krona",
+    rate: 2.9113,
+  },
+  {
+    short: "NOK",
+    name: "Norway krone",
+    rate: 0.4340,
+  },
+  {
+    short: "SEK",
+    name: "Swedish krona",
+    rate: 0.4510,
+  },
+  {
+    short: "HRK",
+    name: "Croatian kuna",
+    rate: 0.6012,
+  },
+  {
+    short: "RON",
+    name: "Romanian leu",
+    rate: 0.9329,
+  },
+  {
+    short: "BGN",
+    name: "Bulgarian lev",
+    rate: 2.3236,
+  },
+  {
+    short: "TRY",
+    name: "Turkish lira",
+    rate: 0.5010,
+  },
+  {
+    short: "ILS",
+    name: "New Israel shekel",
+    rate: 1.1545,
+  },
+  {
+    short: "CLP_100",
+    name: "100 Chilean peso",
+    rate: 0.5258,
+  },
+  {
+    short: "PHP",
+    name: "Philippine peso",
+    rate: 0.0770,
+  },
+  {
+    short: "MXN",
+    name: "Mexican peso",
+    rate: 0.1857,
+  },
+  {
+    short: "ZAR",
+    name: "South Africa rand",
+    rate: 0.2491,
+  },
+  {
+    short: "BRL",
+    name: "Brazilian real",
+    rate: 0.6991,
+  },
+  {
+    short: "MYR",
+    name: "Malaysian ringgit",
+    rate: 0.9219,
+  },
+  {
+    short: "RUB",
+    name: "Russian rouble",
+    rate: 0.0495,
+  },
+  {
+    short: "IDR_10000",
+    name: "10000 Indonesian rupiah",
+    rate: 2.6612,
+  },
+  {
+    short: "INR_100",
+    name: "100 Indian rupiah",
+    rate: 5.0608,
+  },
+  {
+    short: "KRW_100",
+    name: "100 South Korean won",
+    rate: 0.3401,
+  },
+  {
+    short: "CNY",
+    name: "Chinese Yuan renminbi",
+    rate: 0.5731,
+  },
+  {
+    short: "XDR",
+    name: "SDR (MFW)",
+    rate: 5.3547,
+  }
+];
