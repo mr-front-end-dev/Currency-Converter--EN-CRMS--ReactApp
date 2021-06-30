9a47fcc8997cacb178ccd8cf6cd25506f8165955
MYKYTA RYSIEV <nickpolska21@gmail.com>
2021-06-30 14:52:51 +0200

    Added bg-color and padding

--- src/App/Form/Result/styled.js
+++ src/App/Form/Result/styled.js
@@ -3,5 +3,9 @@ import styled from "styled-components";
 export const Wrapper = styled.p`
   font-size: 22px;
   text-align: center;
+  /* ________________________________________________ */
   color: ${({ theme }) => theme.color.lightslategrey};
+  background-color: ${({ theme }) => theme.color.white};
+  padding: 30px;
+  /* ________________________________________________ */
 `;
